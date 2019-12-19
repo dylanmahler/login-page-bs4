61d4c9300a16f12c5bcd9c569ef734e2a4684430
dylanmahler <dylanmmahler@gmail.com>
2019-12-19 14:53:26 -0600

    Updates to lockout and 2FA process
    Updated names of pages for 2FA and delivery method, added lockout page with timer.

--- js/my-login.js
+++ js/my-login.js
@@ -91,3 +91,27 @@ $(function() {
 	});
 	
 });
+
+$(document).ready(function(){
+	const second = 1000,
+      minute = second * 60,
+      hour = minute * 60;
+
+let countDown = new Date('Sep 30, 2020 00:00:00').getTime(),
+    x = setInterval(function() {
+
+      let now = new Date().getTime(),
+          distance = countDown - now;
+
+          document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+      
+      //do something later when date is reached
+      //if (distance < 0) {
+      //  clearInterval(x);
+      //  'IT'S MY BIRTHDAY!;
+      //}
+
+    }, second)
+});
+	
